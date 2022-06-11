0032ed5ff015ecf0e5f9407d346ec7069afef52d
Alastair Zotos <alastairzotos@gmail.com>
2022-06-11 02:44:55 +0100

    female voice

--- src/utils/speech.ts
+++ src/utils/speech.ts
@@ -1,6 +1,8 @@
+const mainVoice = speechSynthesis.getVoices().find(voice => voice.name === 'Samantha');
 
 export const speak = (words: string) => new Promise((resolve) => {
   const utterance = new SpeechSynthesisUtterance(words);
+  utterance.voice = mainVoice!;
   utterance.onend = resolve;
   speechSynthesis.speak(utterance);
 });

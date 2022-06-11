bf56757e17fea8024c420762e1e29ed793ed6ae4
Alastair Zotos <alastairzotos@gmail.com>
2022-06-11 17:52:09 +0100

    default voice

--- src/utils/speech.ts
+++ src/utils/speech.ts
@@ -2,7 +2,6 @@ const mainVoice = speechSynthesis.getVoices().find(voice => voice.name === 'Sama
 
 export const speak = (words: string) => new Promise((resolve) => {
   const utterance = new SpeechSynthesisUtterance(words);
-  utterance.voice = mainVoice!;
   utterance.onend = resolve;
   speechSynthesis.speak(utterance);
 });

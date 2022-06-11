30312dcc4fb15d163bf0dc9c1c2aaa299d6b053f
Alastair Zotos <alastairzotos@gmail.com>
2022-06-11 19:08:59 +0100

    another joke interaction

--- src/plugins/jokes.plugin.ts
+++ src/plugins/jokes.plugin.ts
@@ -1,3 +1,4 @@
+import { CallistoContext } from "../callisto/context";
 import { jokesIntegration } from "../integrations/jokes.integration";
 import { CallistoPlugin } from "../models/service.models";
 
@@ -5,6 +6,14 @@ export const jokesPlugin: CallistoPlugin = ctx => {
   ctx.addInteraction("tell me a joke", async () => {
     const joke = await jokesIntegration.getJoke();
 
-    return joke;
+    return {
+      responseText: joke,
+      context: new CallistoContext(ctx)
+        .addInteraction('tell me another one', async () => await jokesIntegration.getJoke())
+        .addInteraction('and another one', async () => await jokesIntegration.getJoke())
+        .addInteraction('and another', async () => await jokesIntegration.getJoke())
+        .addInteraction('another', async () => await jokesIntegration.getJoke())
+        .addInteraction('another one', async () => await jokesIntegration.getJoke())
+    };
   });
 }

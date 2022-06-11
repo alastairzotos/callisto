fa9773e7d246038c03e4a37bab672aded43255fc
Alastair Zotos <alastairzotos@gmail.com>
2022-06-11 02:23:29 +0100

    update funny plugin

--- src/plugins/funny.plugin.ts
+++ src/plugins/funny.plugin.ts
@@ -1,5 +1,5 @@
 import { CallistoPlugin } from "../models/service.models"
 
 export const funnyPlugin: CallistoPlugin = ctx => {
-  ctx.addInteraction("tell (.+) to go f\\*\\*\\* himself", async ([name]) => `Go fuck yourself ${name}`)
+  ctx.addInteraction("tell (.+) to go f\\*\\*\\* (himself|herself|themselves)", async ([name]) => `Go fuck yourself ${name}`)
 }

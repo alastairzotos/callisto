a9b951b984ed2bef0b77cc59fd413eb04b555a05
Alastair Zotos <alastairzotos@gmail.com>
2022-09-03 20:45:02 +0100

    refactor: use nextjs

---
+++ apps/callisto/env.ts
@@ -0,0 +1,5 @@
+
+export const env = {
+  production: process.env.NEXT_PUBLIC_PRODUCTION === 'true',
+  weatherApiKey: process.env.NEXT_PUBLIC_WEATHER_API_KEY,
+};
--- apps/callisto/src/env.ts
+++
@@ -1,5 +0,0 @@
-
-export const env = {
-  production: process.env.REACT_APP_PROD === 'true',
-  weatherApiKey: process.env.REACT_APP_WEATHER_API_KEY,
-};

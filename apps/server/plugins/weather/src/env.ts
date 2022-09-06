67f5b0b6cd4c56f9d34827a2e974a66892bb5b28
Alastair Zotos <alastairzotos@gmail.com>
2022-09-06 16:27:36 +0100

    chore: weather api

--- apps/server/plugins/weather/src/env.ts
+++ apps/server/plugins/weather/src/env.ts
@@ -2,6 +2,6 @@ import { config } from 'dotenv';
 config();
 
 export const env = {
-  production: process.env.PRODUCTION,
-  weatherApiKey: process.env.WEATHER_API_KEY,
+  production: true, //process.env.PRODUCTION,
+  weatherApiKey: 'b445b81ce6dc4bf09ff215438221006', //process.env.WEATHER_API_KEY,
 };

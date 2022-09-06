db927cff6667c0704a209f4e175dc94909435912
Alastair Zotos <alastairzotos@gmail.com>
2022-09-06 10:57:05 +0100

    chore: housekeeping

--- apps/server/plugins/random-number/src/random-number.plugin.ts
+++ apps/server/plugins/random-number/src/random-number.plugin.ts
@@ -1,3 +1,4 @@
 import { onInteraction } from '@bitmetro/callisto-ipc';
 
 onInteraction('random-number', async () => `${ Math.round(Math.random() * 99) + 1 }`);
+onInteraction('another', async () => `${ Math.round(Math.random() * 99) + 1 }`);

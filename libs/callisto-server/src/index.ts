70c109b341bcf0436005feb0483656c5cd37b4fc
Alastair Zotos <alastairzotos@gmail.com>
2022-09-06 13:26:08 +0100

    build: dockerfile for server

--- libs/callisto-server/src/index.ts
+++ libs/callisto-server/src/index.ts
@@ -9,89 +9,33 @@ import { execSync } from 'child_process';
 import { sendAnswer, sendCommand } from './ipc';
 import { PluginImport, PluginImportSchema, PluginInteraction } from './models';
 
-const addPluginInteractions = (
-  process: ChildProcess,
-  ctx: CallistoContext,
-  interactions: PluginInteraction[],
-  basePath: string,
-) => {
-  interactions.forEach(({ id, prompts, inputs, children, goToParentContextOnceFinished }) => {
-    ctx.addPrompts(prompts || []);
-    ctx.addInteraction(inputs, async params => {
-      try {
-        const result = await sendCommand(process, id, params);
 
-        if (result.type === 'question') {
-          return ask(ctx, result.response, async answer => {
-            const answerResult = await sendAnswer(process, answer);
-            return answerResult.response!;
-          })
-        }
-
-        if (!children || children.length === 0) {
-          return result.response;
-        }
-
-        const subContext = new CallistoContext(ctx);
-        addPluginInteractions(process, subContext, children, basePath);
-
-        return {
-          responseText: result.response,
-          goToParentContextOnceFinished,
-          context: subContext
-        }
-      } catch {
-        return 'There was an error handling your request'
-      }
-    })
-  })
-}
-
-const handlePluginImport = (callisto: Callisto, name: string, configFileContent: string, basePath: string, format: 'json' | 'yaml') => {
-  console.log(`Importing plugin '${name}'`);
-
-  let config: PluginImport =
-    format === 'yaml'
-      ? parseYaml(configFileContent)
-      : JSON.parse(configFileContent);
-
-  console.log(basePath);
-
-  console.log('Installing dependencies');
-  execSync('npm i', { cwd: basePath, stdio: 'inherit' });
-
-  console.log('Building');
-  execSync('npm run build', { cwd: basePath, stdio: 'inherit' });
-
-  callisto.applyPlugin(ctx => {
-    try {
-      const { resolve, interactions } = PluginImportSchema.parse(config) as PluginImport;
-
-      console.log('Starting');
-      const process = forkProcess?.(resolve, basePath);
-
-      addPluginInteractions(process!, ctx, interactions, basePath);
-    } catch (e) {
-      console.error(e)
-    }
-  });
+interface PluginRef {
+  name: string;
+  resolve: string;
+  pluginPath: string;
+  interactions: PluginInteraction[];
 }
 
 export class CallistoServer {
   private wss: ws.WebSocketServer | undefined;
+  private plugins: PluginRef[] = [];
 
   constructor(
     private readonly options: {
       pluginsRoot: string
     }
-  ) {}
+  ) { }
 
   start(port = 8080) {
+    this.importPlugins();
+
     this.wss = new ws.WebSocketServer({ port });
 
     this.wss.on('connection', ws => {
-      const callisto = this.createCallistoInstance();
-      
+      const callisto = new Callisto();
+      this.plugins.forEach(plugin => this.applyPlugin(callisto, plugin));
+
       ws.on('message', async msg => {
         const { error, interactionResponse } = await callisto.handleInput(msg.toString());
         ws.send(JSON.stringify({
@@ -111,20 +55,84 @@ export class CallistoServer {
     console.log(`Callisto server running on ws://localhost:${port}`);
   }
 
-  private createCallistoInstance() {
-    const callisto = new Callisto();
-  
-    const importPlugin = (pluginFile: string) => {
-      const pluginPath = path.dirname(pluginFile);
-      const pluginFileContent = fs.readFileSync(pluginFile).toString();
-  
-      handlePluginImport(callisto, path.basename(pluginPath), pluginFileContent, pluginPath, pluginFile.endsWith('.yaml') || pluginFile.endsWith('.yml') ? 'yaml' : 'json');
-    }
-  
+  private importPlugins() {
     fs.readdirSync(this.options.pluginsRoot)
       .map(pluginName => path.resolve(this.options.pluginsRoot, pluginName, 'plugin.yaml'))
-      .forEach(importPlugin);
+      .forEach(pluginFile => this.importPlugin(pluginFile));
+  }
+
+  private importPlugin(pluginFile: string) {
+    const pluginPath = path.dirname(pluginFile);
+    const pluginFileContent = fs.readFileSync(pluginFile).toString();
+    const name = path.basename(pluginPath);
+    const format = pluginFile.endsWith('.yaml') || pluginFile.endsWith('.yml') ? 'yaml' : 'json';
+
+    console.log(`Importing plugin ${name}`);
+
+    try {
+      let config: PluginImport =
+        format === 'yaml'
+          ? parseYaml(pluginFileContent)
+          : JSON.parse(pluginFileContent);
+
+      const { resolve, interactions } = PluginImportSchema.parse(config) as PluginImport;
+
+      console.log('Installing dependencies');
+      execSync('npm i', { cwd: pluginPath, stdio: 'inherit' });
+
+      console.log('Building');
+      execSync('npm run build', { cwd: pluginPath, stdio: 'inherit' });
+
+      this.plugins.push({ name, resolve, pluginPath, interactions })
+
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  private applyPlugin(callisto: Callisto, { name, resolve, pluginPath, interactions }: PluginRef) {
+    callisto.applyPlugin(ctx => {
+      const process = forkProcess?.(resolve, pluginPath);
+
+      this.addPluginInteractions(process!, ctx, interactions, pluginPath);
+    });
+  }
+
+  private addPluginInteractions(
+    process: ChildProcess,
+    ctx: CallistoContext,
+    interactions: PluginInteraction[],
+    basePath: string,
+  ) {
+    interactions.forEach(({ id, prompts, inputs, children, goToParentContextOnceFinished }) => {
+      ctx.addPrompts(prompts || []);
+      ctx.addInteraction(inputs, async params => {
+        try {
+          const result = await sendCommand(process, id, params);
   
-    return callisto;
+          if (result.type === 'question') {
+            return ask(ctx, result.response, async answer => {
+              const answerResult = await sendAnswer(process, answer);
+              return answerResult.response!;
+            })
+          }
+  
+          if (!children || children.length === 0) {
+            return result.response;
+          }
+  
+          const subContext = new CallistoContext(ctx);
+          this.addPluginInteractions(process, subContext, children, basePath);
+  
+          return {
+            responseText: result.response,
+            goToParentContextOnceFinished,
+            context: subContext
+          }
+        } catch {
+          return 'There was an error handling your request'
+        }
+      })
+    })
   }
 }

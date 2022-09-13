6817a9cb49fad595f9ed73576afebb507ea1fd32
Alastair Zotos <alastairzotos@gmail.com>
2022-09-13 19:44:47 +0300

    refactor: remove default plugins

--- apps/server/plugins/random-number-1.0.0/src/random-number.plugin.ts
+++
@@ -1,4 +0,0 @@
-import { onInteraction } from '@bitmetro/callisto-plugins';
-
-onInteraction('random-number', async () => `${ Math.round(Math.random() * 99) + 1 }`);
-onInteraction('another', async () => `${ Math.round(Math.random() * 99) + 1 }`);
--- apps/server/plugins/weather-1.0.0/src/env.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-export const env = {
-  production: true, //process.env.PRODUCTION,
-  weatherApiKey: 'b445b81ce6dc4bf09ff215438221006', //process.env.WEATHER_API_KEY,
-};

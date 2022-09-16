57b87df3e5351fd6b4fbaedc6b5bca06e829565a
Alastair Zotos <alastairzotos@gmail.com>
2022-09-16 18:35:30 +0300

    chore: some typing fixes

--- libs/callisto-server/src/server.ts
+++ libs/callisto-server/src/server.ts
@@ -55,7 +55,7 @@ export class CallistoServer {
     this.logger.log(`Callisto server running on ${chalk.gray(`ws://localhost:${port}`)}`);
   }
 
-  createExpressServer() {
+  createExpressServer(): express.Express {
     const app = express();
 
     app.use(cors());

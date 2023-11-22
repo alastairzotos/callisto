badb3d47a530976f32c80006b702cf3439bc8777
Alastair Zotos <alastairzotos@gmail.com>
2023-11-22 15:18:46 +0200

    feat: different plugins path for prod and dev

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,8 @@
 import { CallistoServer } from '@bitmetro/callisto-server';
 import * as path from 'path';
 
-const server = new CallistoServer({
-  pluginsRoot: path.resolve(__dirname, '..', 'plugins')
-});
+const pluginsRoot = process.env.NODE_ENV === 'production' ? '/plugins' : path.resolve(__dirname, '..', 'plugins');
+
+const server = new CallistoServer({ pluginsRoot });
 
 server.start();
